d039d3df68794d64b4401abc527d5b52313e9dae
Alexander Strizhak <gam6itko@gmail.com>
2025-11-07 14:21:43 +0300

    up

--- src/Metrics.php
+++ src/Metrics.php
@@ -25,6 +25,7 @@ use Symfony\Component\Config\Definition\Processor;
  *     name: string,
  *     version: string
  * }
+ *
  * @psalm-type TMetric = array{
  *     name: string,
  *     type?: string,
@@ -34,20 +35,24 @@ use Symfony\Component\Config\Definition\Processor;
  *     feature_flags?: array,
  *     utc_startup_timestamp: int
  * }
+ *
  * @psalm-type TLabel = array{
  *     key: non-empty-string,
  *     value: string
  * }
+ *
  * @psalm-type TItem = array{
  *     name: string,
  *     value: non-negative-int,
  *     unit: mixed,
  *     labels: list<TLabel>
  * }
+ *
  * @psalm-type TMeta = array{
  *     window_size_seconds: int,
  *     utc_now_timestamp: positive-int
  * }
+ *
  * @psalm-type TRemediationComponents = array{
  *     name: string,
  *     type?: string,
@@ -80,8 +85,8 @@ class Metrics
     private $properties;
 
     /**
-     * @param TMetric     $properties
-     * @param TMeta       $meta
+     * @param TMetric $properties
+     * @param TMeta $meta
      * @param list<TItem> $items
      */
     public function __construct(
